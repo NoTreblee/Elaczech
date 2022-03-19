9652d43ec2f1b3e4ab27ca1b8fbdf7e9cd58862d
NoTreblee <fiercestword@gmail.com>
2022-03-19 20:10:38 +0100

    added back on top button

---
+++ script.js
@@ -0,0 +1,5 @@
+mybutton = document.getElementById('myBtn');
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
