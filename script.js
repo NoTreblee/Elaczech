ad3d9964356bee59ab1592370edc0ef3ed4a73be
NoTreblee <42041720+NoTreblee@users.noreply.github.com>
2022-04-05 22:02:07 +0200

    .

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-function openNav() {
-  document.getElementById('mySidenav').style.height = '230px';
-  document.getElementById('header').style.marginTop = '230px';
-  document.getElementById('main').style.marginTop = '230px';
+let mySidenav = document.getElementById('mySidenav');
+let header = document.getElementById('header');
+
+document.getElementById('navButton').addEventListener('click', () => {
+  navOpen();
+  headerMargin();
+});
+function headerMargin() {
+  header.classList.toggle('headertop');
 }
-function closeNav() {
-  document.getElementById('mySidenav').style.height = '0';
-  document.getElementById('header').style.marginTop = '0';
-  document.getElementById('main').style.marginTop = '0';
+function navOpen() {
+  mySidenav.classList.toggle('opened');
 }
